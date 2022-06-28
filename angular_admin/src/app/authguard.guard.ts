196996b7fd192f8b42c9b2f73a960601292efcf5
thamim358 <thamimansari358@gmail.com>
2022-06-28 18:04:44 +0530

    login

---
+++ angular_admin/src/app/authguard.guard.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { ApiService } from './api.service';
+
+@Injectable({
+providedIn: 'root'
+})
+
+export class AuthguardGuard implements CanActivate {
+constructor(private dataService: ApiService,private router: Router ) {}
+canActivate(
+route: ActivatedRouteSnapshot,
+state: RouterStateSnapshot): boolean {
+const routeurl: string = state.url;
+return this.isLogin(routeurl);
+}
+
+isLogin(routeurl: string) {
+if (this.dataService.isLoggedIn()) {
+return true;
+}
+else return false;
+
+this.dataService.routectUrl = routeurl;
+this.router.navigate(['/login'], {queryParams: { returnUrl: routeurl }} );
+}
+}
